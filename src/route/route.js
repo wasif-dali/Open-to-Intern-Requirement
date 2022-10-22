fc6b03382959888394ac0dea2c332085e45827c5
Wasif Ali <you@example.com>
2022-10-23 01:00:47 +0530

    done

---
+++ src/route/route.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router  = express.Router()
+const collageController = require("../Controller/CollegeController")
+const getcontroller = require("../Controller/getController")
+const internscontroller = require("../Controller/InternController")
+
+
+router.post("/functionup/colleges",collageController.createcollage)
+router.post ("/functionup/interns",internscontroller.createinterns)
+router.get("/functionup/collegeDetails",getcontroller.getcollegeDetails)
+
+
+module.exports = router;
